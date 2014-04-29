2db92088b7f914d8f863eece316fa39faa9f0315
Nick Gartmann <nick@rokkincat.com>
2014-04-29 12:53:45 -0500

    better database handling (based on env)

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,8 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("postgres://localhost/rokkincrm_development", :loggers => [logger])
-  when :production  then Sequel.connect("postgres://localhost/rokkincrm_production",  :loggers => [logger])
+when :development then Sequel.connect(ENV['DATABASE_URL'] || "postgres://localhost/rokkincrm_development", :loggers => [logger])
+when :production  then Sequel.connect(ENV['DATABASE_URL'],  :loggers => [logger])
   when :test        then Sequel.connect("postgres://localhost/rokkincrm_test",        :loggers => [logger])
 end
 Sequel::Model.db.extension :pg_array
