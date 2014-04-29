b8666c2f9a514082109d2d2e80ff032527a564ac
Greg Billetdeaux <gregadeaux@gmail.com>
2014-04-29 14:24:46 -0500

    Tabs to spaces

--- db/migrate/004_add_user_to_message.rb
+++ db/migrate/004_add_user_to_message.rb
@@ -1,12 +1,12 @@
 Sequel.migration do
   up do
-  	alter_table :messages do
+    alter_table :messages do
       add_column :user_id, Integer
     end
   end
 
   down do
-  	alter_table :messages do
+    alter_table :messages do
       drop_column :user_id
     end
   end
